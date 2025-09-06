8d60f39b250c0717e71538a9cb5df3b832184dbc
Md Rahat Islam <islammdrahat308@gmail.com>
2025-09-06 22:17:49 +0600

    express fun

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,26 +1,32 @@
 const getContacts = (req, res) => {
-  res.status(200).json({ message: "get all contacts" });
+ res.status(200).json({ message: "get all contacts" });
 };
 const createContact = (req, res) => {
-  // console.log("create contect", req);
-  console.log("create contect", req.body);
+ // console.log("create contect", req);
 
-  res.status(201).json({ message: "create contact" });
+ const { name, age, phone } = req.body;
+ if (!name || !age || !phone) {
+  res.status(400);
+  throw new Error("All fields are require");
+ }
+ console.log("create contect", req.body);
+
+ res.status(201).json({ message: "create contact" });
 };
 const getContact = (req, res) => {
-  res.status(200).json({ message: `get contact for ${req.params.id}` });
+ res.status(200).json({ message: `get contact for ${req.params.id}` });
 };
 const updateContact = (req, res) => {
-  res.status(200).json({ message: `update contacts for ${req.params.id}` });
+ res.status(200).json({ message: `update contacts for ${req.params.id}` });
 };
 const deleteContact = (req, res) => {
-  res.status(200).json({ message: `delete contacts for ${req.params.id}` });
+ res.status(200).json({ message: `delete contacts for ${req.params.id}` });
 };
 
 module.exports = {
-  getContacts,
-  createContact,
-  getContact,
-  updateContact,
-  deleteContact,
+ getContacts,
+ createContact,
+ getContact,
+ updateContact,
+ deleteContact,
 };
